dc0de9f60ac148b0a66d7cbc41c54feee3731711
Katerina Bosko <katerinabosko@gmail.com>
2022-10-10 15:11:03 -0700

    configured development environment

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-console':0,
+        'no-unused-vars': 1,
+        'no-var': 1,
+    }
+};
