e5cf4ab0fc01139ee1f7c2e03e8fd342c5dfe7db
Katerina Bosko <katerinabosko@gmail.com>
2022-10-18 18:58:33 -0700

    added router to myhabits and myhabits.html starter code

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
-import express from 'express';
+import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
-const router = express.Router();
+const router = express.Router()
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.sendFile(join(__dirname, "..", "public", "index.html"));
+});
+
+/* GET myhabits page. */
+router.get("/myhabits", function (req, res, next) {
+  res.sendFile(join(__dirname, "..", "public", "myhabits.html"));
 });
 
 export default router;
